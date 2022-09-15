e2d85abd9a71dbdaa9a06b7f05c6f26405394e26
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-09-15 09:00:31 -0300

    Aula Teorica

---
+++ Teoria/MaterialTeorico.js
@@ -0,0 +1,5 @@
+// Template Expressions: é o recurso que nos permite executar js no jsx e também interpolar variáveis. Sintaxe: {algumCódigoemJs} - tudo que está entre chaves é processado pelo javaScript e nos retorna um resultado.
+
+//Hierarquia de  Componentes: os componentes podem ser reutilizados em vários componentes, e ainda podem formar uma hierarquia, sendo importados uns dentro dos outros.
+
+//Eventos: são utilizados com a função OnNomedoEvento, por exemplo o evento de click - onClick. As funções geralmente tem o padrão handleAlgumaCoisa.
